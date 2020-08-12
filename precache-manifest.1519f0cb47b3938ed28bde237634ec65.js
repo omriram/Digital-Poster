aa7bb3ec3ab7347cd0a581785032086f6548b1a9
Omri Ram <omriram08@gmail.com>
2020-08-12 16:44:42 +0300

    Updates

---
+++ precache-manifest.1519f0cb47b3938ed28bde237634ec65.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ee90eb3dc92e767913e8b7fdf22bc7",
+    "url": "/Digital-Poster/index.html"
+  },
+  {
+    "revision": "bdf76974ae2e9d7f72fa",
+    "url": "/Digital-Poster/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aabc9e2b2b1667b6f1d9",
+    "url": "/Digital-Poster/static/css/main.0937ef40.chunk.css"
+  },
+  {
+    "revision": "bdf76974ae2e9d7f72fa",
+    "url": "/Digital-Poster/static/js/2.3633875e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Digital-Poster/static/js/2.3633875e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aabc9e2b2b1667b6f1d9",
+    "url": "/Digital-Poster/static/js/main.88775056.chunk.js"
+  },
+  {
+    "revision": "6aead396c1e13e5769b4",
+    "url": "/Digital-Poster/static/js/runtime-main.79d9f7df.js"
+  },
+  {
+    "revision": "c578fd523c8756293ad8581da5ebdb60",
+    "url": "/Digital-Poster/static/media/azal.c578fd52.png"
+  },
+  {
+    "revision": "106fbcbcbd1ed5a75e022e98de2115a7",
+    "url": "/Digital-Poster/static/media/beach.106fbcbc.jpg"
+  },
+  {
+    "revision": "4a900c167c2e52afd109e7f4aa807ce8",
+    "url": "/Digital-Poster/static/media/coffee1.4a900c16.jpg"
+  },
+  {
+    "revision": "c78279e6aefd215b212fa20f71b0e112",
+    "url": "/Digital-Poster/static/media/ice1.c78279e6.jpg"
+  },
+  {
+    "revision": "6b73596aa80e1fd644df4a816dc46b62",
+    "url": "/Digital-Poster/static/media/logo.6b73596a.png"
+  },
+  {
+    "revision": "e794af62b2f0e3df86f964deb0e92238",
+    "url": "/Digital-Poster/static/media/mag.e794af62.png"
+  },
+  {
+    "revision": "3f676d4f1eb5c643dd6eae3b2726a285",
+    "url": "/Digital-Poster/static/media/mivza.3f676d4f.png"
+  },
+  {
+    "revision": "85ec9514145b8e31a01ce41e5b60d90f",
+    "url": "/Digital-Poster/static/media/new.85ec9514.png"
+  },
+  {
+    "revision": "888009e976727542da0fc47f199fea88",
+    "url": "/Digital-Poster/static/media/price-tag.888009e9.png"
+  }
+]);
